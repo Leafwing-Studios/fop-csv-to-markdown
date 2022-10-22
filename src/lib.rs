a3bfc4d11f3751659c3104cd6a64bdbd60a8f04d
plof27 <rosepeck1997@gmail.com>
2022-10-22 18:19:18 -0400

    Add recursive mode
    With many hard coded edge cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-pub fn process_file(filename: String, header_field_index: usize, header_level: usize) -> String {
+use std::{fs, path::PathBuf};
+
+pub fn process_file(filename: &PathBuf, header_field_index: usize, header_level: usize) -> String {
     let mut reader = csv::Reader::from_path(filename).expect("Failure to read CSV file");
     let headers = reader.headers().expect("Failure getting headers").clone();
 
@@ -6,7 +8,7 @@ pub fn process_file(filename: String, header_field_index: usize, header_level: u
         .records()
         .map(|row| {
             let mut result_str = String::new();
-            let row = row.expect("Failure parsing string");
+            let row = row.expect(&format!("Failure parsing string for file {:?}", filename));
 
             let header = row.get(header_field_index).unwrap_or("Unnamed");
             result_str.push_str(&format!("{} {}\n", "#".repeat(header_level), header));
@@ -28,4 +30,36 @@ pub fn process_file(filename: String, header_field_index: usize, header_level: u
         .collect();
 
     entries.join("")
+}
+
+pub fn process_dir(filename: PathBuf, header_field_index: usize, header_level: usize) {
+    let dir_contents = fs::read_dir(filename).expect("Failure reading directory");
+
+    for path in dir_contents {
+        let path = path.expect("Failure reading path");
+
+        if path
+            .metadata()
+            .expect("Failure reading path metadata")
+            .is_file()
+        {
+            if let Some(extension) = path.path().extension() {
+                if extension == "csv" {
+                    let mut path = path.path();
+
+                    let header_field_index =
+                        if path.file_name().unwrap().to_str().unwrap() == "features.csv" {
+                            1
+                        } else {
+                            header_field_index
+                        };
+                    let markdown = process_file(&path, header_field_index, header_level);
+                    path.set_extension("md");
+                    fs::write(path, markdown).expect("Failure writing output file");
+                }
+            }
+        } else {
+            process_dir(path.path(), header_field_index, header_level);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 
 use clap::Parser;
-use fop_csv_to_markdown::process_file;
+use fop_csv_to_markdown::{process_dir, process_file};
 
 #[derive(Parser, Debug)]
 struct Args {
-    /// CSV file to process
-    in_file: String,
+    /// CSV file or directory to process.
+    /// If given a directory, the directory is searched recursively,
+    /// and the transformation is applied to all CSV files found.
+    in_path: PathBuf,
 
     /// Index of the column to be used as the "header" for the final markdown snippet
     #[arg(short = 'h', long, default_value_t = 0)]
@@ -16,11 +18,14 @@ struct Args {
     #[arg(short = 'l', long, default_value_t = 2)]
     header_level: usize,
 
-    /// Filename to output finished markdown file to. If not provided, outputs the result to stdout.
+    /// Filename to output finished markdown file to.
+    /// If not provided, outputs the result to stdout.
+    /// If [`in_path`] is a directory, this is ignored
     #[arg(short, long)]
-    out_file: Option<String>,
+    out_file: Option<PathBuf>,
 
     /// Title to prepend to the top of the output
+    /// If [`in_path`] is a directory, this is ignored
     #[arg(short, long)]
     title: Option<String>,
 }
@@ -28,15 +33,24 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let mut markdown = process_file(args.in_file, args.header_field_index, args.header_level);
+    if args
+        .in_path
+        .metadata()
+        .expect("Failure returing path metadata")
+        .is_file()
+    {
+        let mut markdown = process_file(&args.in_path, args.header_field_index, args.header_level);
 
-    if let Some(title) = args.title {
-        markdown = format!("# {}\n\n", title) + &markdown;
-    }
+        if let Some(title) = args.title {
+            markdown = format!("# {}\n\n", title) + &markdown;
+        }
 
-    if let Some(path) = args.out_file {
-        fs::write(path, markdown).expect("Failure writing output file");
+        if let Some(path) = args.out_file {
+            fs::write(path, markdown).expect("Failure writing output file");
+        } else {
+            println!("{}", markdown);
+        }
     } else {
-        println!("{}", markdown);
+        process_dir(args.in_path, args.header_field_index, args.header_level);
     }
 }
